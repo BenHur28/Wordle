b5d8b250ab6041f0b6fc193f4376e2e51a6e25aa
Ben Hur <bhur777@gmail.com>
2023-09-22 18:08:39 -0400

    Add "WORDLE" heading

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,11 @@ import Wordle from "./components/wordle";
 
 export default function Home() {
 	return (
-		<div>
-			<Wordle></Wordle>
+		<div className="h-100 w-100 flex flex-col justify-center items-center">
+			<div className="border-b w-full text-center">
+				<h1 className="text-black text-3xl font-semibold py-2">WORDLE</h1>
+			</div>
+			<Wordle />
 		</div>
 	);
 }
