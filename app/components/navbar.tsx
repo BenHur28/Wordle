bd561bb5379fae755c6c5e9f811c037da4919d29
Ben Hur <bhur777@gmail.com>
2023-09-22 18:11:28 -0400

    refactor header into navbar component

---
+++ app/components/navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+	return (
+		<nav className="border-b w-full text-center">
+			<h1 className="text-black text-3xl font-semibold py-2">WORDLE</h1>
+		</nav>
+	);
+};
+
+export default Navbar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
+import Navbar from "./components/navbar";
 import Wordle from "./components/wordle";
 
 export default function Home() {
 	return (
 		<div className="h-100 w-100 flex flex-col justify-center items-center">
-			<div className="border-b w-full text-center">
-				<h1 className="text-black text-3xl font-semibold py-2">WORDLE</h1>
-			</div>
+			<Navbar />
 			<Wordle />
 		</div>
 	);
