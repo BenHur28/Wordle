506bbb1f8aa1ce761b26bf241c196bb5c4c7e0d2
Ben Hur <bhur777@gmail.com>
2023-09-22 21:09:18 -0400

    add submittedgusses and display previous guesses

--- app/components/wordle.tsx
+++ app/components/wordle.tsx
@@ -56,7 +56,34 @@ const Wordle = () => {
 		);
 	};
 
-	const PreviousGuess = () => {};
+	type SubmittedGuessesProps = {
+		submittedGuesses: string[][];
+	};
+
+	const SubmittedGuesses = ({ submittedGuesses }: SubmittedGuessesProps) => {
+		return (
+			<>
+				{submittedGuesses.map((guess, i) => {
+					return <GuessRow key={i} guess={guess} />;
+				})}
+			</>
+		);
+	};
+
+	const GuessRow = ({ guess }: CurrentGuessProps) => {
+		return (
+			<div className="flex gap-x-2 mb-2">
+				{Array.from({ length: 5 }).map((_, i) => (
+					<span
+						className="flex justify-center items-center border-2 border-solid-black text-4xl h-16 w-16"
+						key={i}
+					>
+						{guess[i]}
+					</span>
+				))}
+			</div>
+		);
+	};
 
 	const EmptyGuess = () => {
 		return (
@@ -74,6 +101,7 @@ const Wordle = () => {
 	return (
 		<div>
 			<div className="mt-10">
+				<SubmittedGuesses submittedGuesses={submittedGuesses} />
 				<CurrentGuess guess={guess} />
 				{Array.from({
 					length: totalGuessMax - submittedGuesses.length - 1,
