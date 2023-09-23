00bfbc225a0ece791f4509ea95a663f7e619fb4b
Ben Hur <bhur777@gmail.com>
2023-09-23 19:19:48 -0400

    Refactor and Finish guessrow to complete game

--- app/components/Guesses.tsx
+++ app/components/Guesses.tsx
@@ -49,22 +49,37 @@ const SubmittedGuesses = ({
 };
 
 const GuessRow = ({ guess, wordOfDay, charMap }: CurrentGuessProps) => {
+	const tempMap = { ...charMap };
+
+	guess.forEach((char, i) => {
+		const isCorrect = wordOfDay[i] === char;
+		if (isCorrect) {
+			tempMap[char] -= 1;
+		}
+	});
+
 	return (
 		<div className="flex gap-x-2 mb-2">
-			{Array.from({ length: 5 }).map((_, i) => (
-				<span
-					className={cn(
-						"flex justify-center items-center border-2 border-solid-black text-4xl h-16 w-16",
-						guess[i] === wordOfDay[i] ? "bg-green-800" : "",
-						!(guess[i] === wordOfDay[i]) && !!charMap[guess[i]]
-							? "bg-yellow-500"
-							: ""
-					)}
-					key={i}
-				>
-					{guess[i].toLocaleUpperCase()}
-				</span>
-			))}
+			{Array.from({ length: 5 }).map((_, i) => {
+				const isCorrect = guess[i] === wordOfDay[i];
+				let isPresent = false;
+				if (!isCorrect && tempMap[guess[i]]) {
+					isPresent = true;
+					tempMap[guess[i]] -= 1;
+				}
+				return (
+					<span
+						className={cn(
+							"flex justify-center items-center border-2 border-solid-black text-4xl h-16 w-16",
+							isCorrect ? "bg-green-800" : "",
+							isPresent ? "bg-yellow-500" : ""
+						)}
+						key={i}
+					>
+						{guess[i].toLocaleUpperCase()}
+					</span>
+				);
+			})}
 		</div>
 	);
 };
