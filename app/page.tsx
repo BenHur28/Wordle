213bc6fe0a917132ca6987d317c818a89ebe527c
Ben Hur <bhur777@gmail.com>
2023-09-24 21:18:15 -0400

    Add Api route to get a random word of the day

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,23 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import Navbar from "./components/navbar";
 import Wordle from "./components/wordle";
 
 export default function Home() {
+	const [wordOfDay, setWord] = useState<string>("");
+	useEffect(() => {
+		async function fetchWord() {
+			const response = await fetch("/api/word").then((res) => res.json());
+			setWord(response);
+		}
+		fetchWord();
+	}, []);
+
 	return (
 		<div className="h-100 w-100 flex flex-col justify-center items-center">
 			<Navbar />
-			<Wordle wordOfDay="abbey" />
+			<Wordle wordOfDay={wordOfDay} />
 		</div>
 	);
 }
