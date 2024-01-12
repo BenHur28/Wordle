1c03ef57a28270c580f768eef8102a64f4db371e
Ben Hur <bhur777@gmail.com>
2024-01-11 20:20:44 -0500

    small edit

--- __tests__/page.test.tsx
+++ __tests__/page.test.tsx
@@ -1,13 +1,14 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
-import Page from "../app/page";
 import Navbar from "@/app/components/navbar";
 import Wordle from "@/app/components/wordle";
 
 describe("Navbar", () => {
 	it("renders the header", () => {
 		render(<Navbar />);
+
 		const header = screen.getByRole("heading");
+
 		expect(header).toBeInTheDocument;
 	});
 });
